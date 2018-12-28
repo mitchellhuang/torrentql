485aac03d2ec716ad30f4feb5d911d6d6d7283ff
Mitchell Huang <mitchell@mitchellhuang.net>
2018-12-28 00:08:52 -0500

    Allow apollo-server introspection

--- packages/graphql/server.js
+++ packages/graphql/server.js
@@ -1,16 +1,21 @@
-require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers });
 const port = parseInt(process.env.PORT, 10) || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true
+});
 
 const app = express();
 server.applyMiddleware({ app });
 
 if (!process.env.LAMBDA) {
+  require('dotenv').config();
   require('./lib/knex');
   app.listen({ port }, () =>
     console.log(`> Ready on http://localhost:${port}`),
