5ae861b4a83b4aca0ca34f45888ab1b0f3670d18
Mitchell Huang <mitchell@mitchellhuang.net>
2018-12-28 01:21:35 -0500

    Fix

--- packages/graphql/lambda.js
+++ packages/graphql/lambda.js
@@ -1,5 +1,4 @@
 const serverless = require('serverless-http');
-const mysql = require('serverless-mysql');
 const server = require('./server');
 const knex = require('./lib/knex');
 
@@ -7,7 +6,5 @@ const handler = serverless(server);
 
 exports.handler = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const resp = await handler(event, context);
-  await mysql.end();
-  return resp;
+  return await handler(event, context);
 }
