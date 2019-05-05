1ca1bf57ccac128cb32f4da62c2385242d175363
Mitchell Huang <mitchell@mitchellhuang.net>
2019-05-04 23:11:19 -0700

    Add deluge to addTorrent

--- packages/graphql/mutations/addTorrent.js
+++ packages/graphql/mutations/addTorrent.js
@@ -1,20 +1,42 @@
+const _ = require('lodash');
 const uuid = require('uuid/v4');
+const { Deluge } = require('@ctrl/deluge');
+const parseTorrent = require('parse-torrent');
 const Torrent = require('../models/Torrent');
+const Server = require('../models/Server');
 
-const addTorrent = async (parent, args, context) => {
+const addTorrent = async (parent, { data }, context) => {
   if (!context.user) {
     throw new Error('You are not authenticated.');
   }
-  const { magnet, file } = args;
-  const torrent = await Torrent.query().insert({
+  const servers = await Server.query();
+  const server = _.sample(servers);
+  if (!server) {
+    throw new Error('No server available');
+  }
+  const deluge = new Deluge({
+    baseUrl: `${server.protocol}://${server.host}:${server.port}/`,
+    password: 'deluge',
+  });
+  let hash;
+  let type;
+  if (data.includes('magnet')) {
+    type = 'magnet';
+    hash = parseTorrent(data).infoHash;
+    await deluge.addTorrentMagnet(data);
+  } else {
+    type = 'file';
+    hash = parseTorrent(Buffer.from(data, 'base64')).infoHash;
+  }
+  return Torrent.query().insert({
     id: uuid(),
+    hash,
+    type,
+    data,
     status: 'active',
-    file,
-    magnet,
-    server_id: 'gra001',
+    server_id: server.id,
     user_id: context.user.id,
   });
-  return torrent;
 };
 
 module.exports = addTorrent;
--- packages/graphql/seeds/servers.js
+++ packages/graphql/seeds/servers.js
@@ -1,6 +1,6 @@
 exports.seed = function (knex) {
   return knex('servers').del()
     .then(() => knex('servers').insert([
-      { id: 'gra001', host: 'localhost', port: '8112', region: 'eu-west-1' },
+      { id: 'gra001', host: 'localhost', port: '8112', protocol: 'http', region: 'eu-west-1' },
     ]));
 };
