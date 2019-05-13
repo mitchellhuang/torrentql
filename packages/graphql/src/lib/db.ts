5b053976968d488fcfee08dcaab697eb26f7bfa2
Mitchell Huang <mitchell@mitchellhuang.net>
2019-05-12 17:38:59 -0700

    Simplify db.init

--- packages/graphql/src/lib/db.ts
+++ packages/graphql/src/lib/db.ts
@@ -1,18 +1,16 @@
 import { createConnection } from 'typeorm';
 import { join } from 'path';
 
-export const init = async () => {
-  return createConnection({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) | 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    ssl: process.env.DB_SSL === 'true',
-    entities: [
-      join(__dirname, '../entities/*.js'),
-    ],
-    synchronize: true,
-  });
-};
+export const init = () => createConnection({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) | 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: process.env.DB_SSL === 'true',
+  entities: [
+    join(__dirname, '../entities/*.js'),
+  ],
+  synchronize: true,
+});
