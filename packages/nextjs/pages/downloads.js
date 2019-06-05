054dcd60bfebc66eb608f32b7757aba3fb9a59c9
Mitchell Huang <mitchell@mitchellhuang.net>
2019-06-04 23:58:23 -0700

    Fix naming

--- packages/nextjs/pages/downloads.js
+++ packages/nextjs/pages/downloads.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Dashboard from '../layouts/Dashboard';
 
-const Torrents = () => (
-  <Dashboard title="Torrents" noFooter>
+const Downloads = () => (
+  <Dashboard title="Downloads" noFooter>
     <div>
       Downloads.
     </div>
   </Dashboard>
 );
 
-export default Torrents;
+export default Downloads;
