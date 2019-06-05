d9fa8ab39befa62c1138bdfa0fcd60d25f1f0a7c
Mitchell Huang <mitchell@mitchellhuang.net>
2019-06-04 17:03:49 -0700

    Remove footer from dashboard

--- packages/nextjs/pages/dashboard.js
+++ packages/nextjs/pages/dashboard.js
@@ -42,7 +42,7 @@ const ME_QUERY = gql`
 `;
 
 const Dashboard = () => (
-  <Main title="Dashboard">
+  <Main title="Dashboard" noFooter>
     <div className="wrapper">
       <ToolBar />
       <Torrents />
