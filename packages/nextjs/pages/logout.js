10977b1fe7de5ab962762f6c40fb540cb0e3bc85
Mitchell Huang <mitchell@mitchellhuang.net>
2019-06-05 00:09:31 -0700

    Add ssr to /logout

--- packages/nextjs/pages/logout.js
+++ packages/nextjs/pages/logout.js
@@ -3,6 +3,14 @@ import cookie from 'cookie';
 import Router from 'next/router';
 
 class Logout extends Component {
+  static getInitialProps({ res }) {
+    if (!process.browser) {
+      res.clearCookie('token');
+      res.redirect('/login');
+    }
+    return {};
+  }
+
   async componentDidMount() {
     const { client } = this.props;
     await client.resetStore();
