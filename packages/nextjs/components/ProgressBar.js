bf41c1bcdb66de9fb8c77fd2c47a2f623c01871c
pkrisko <patrick.krisko@gmail/com>
2019-06-08 19:20:28 -0400

    Added lil animation

--- packages/nextjs/components/ProgressBar.js
+++ packages/nextjs/components/ProgressBar.js
@@ -18,8 +18,20 @@ const ProgressBar = ({
           width: ${progress}%;
           background-color: ${color || 'var(--primary)'};
           height: 20px;
+          background-image: linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent);
+          background-size: 1rem 1rem;
+          animation-name: progress-bar-stripes;
+          animation-duration: 3s;
+          animation-timing-function: linear;
+          animation-iteration-count: infinite;
           text-align: center;
           min-width: 50px;
+          display: flex;
+          flex-direction: row;
+        }
+        @keyframes progress-bar-stripes {
+          from { background-position: 115px 0; }
+          to { background-position: 0 0; }
         }
         .rounded {
           border-radius: 15px;
