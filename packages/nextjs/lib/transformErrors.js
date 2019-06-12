31c66750ec3dd63afcc63da40a693c4c99e7d9be
Mitchell Huang <mitchell@mitchellhuang.net>
2019-06-12 15:46:26 -0400

    Make transformErrors more verbose

--- packages/nextjs/lib/transformErrors.js
+++ packages/nextjs/lib/transformErrors.js
@@ -1,16 +1,21 @@
 const transformErrors = (err) => {
   err = err.graphQLErrors[0];
-  if (err.message === 'Argument Validation Error') {
-    const result = {};
-    const { validationErrors } = err.extensions.exception;
-    validationErrors.forEach((error) => {
-      // eslint-disable-next-line
-      result[error.property] = Object.values(error.constraints).map(s => s.charAt(0).toUpperCase() + s.slice(1) + '.');
-    });
-    return result;
+  if (err) {
+    if (err.message === 'Argument Validation Error') {
+      const result = {};
+      const { validationErrors } = err.extensions.exception;
+      validationErrors.forEach((error) => {
+        // eslint-disable-next-line
+        result[error.property] = Object.values(error.constraints).map(s => s.charAt(0).toUpperCase() + s.slice(1) + '.');
+      });
+      return result;
+    }
+    return {
+      error: err.message,
+    };
   }
   return {
-    error: err.message,
+    error: 'An unknown error occurred.',
   };
 };
 
