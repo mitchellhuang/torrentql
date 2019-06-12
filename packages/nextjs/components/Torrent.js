788cf6645d887562d6522896e9e953532c017945
jofsky <josh_kofsky@gmail.com>
2019-06-11 18:30:01 -0700

    removing ProgressBar component and fixing up in Torrent.js

--- packages/nextjs/components/Torrent.js
+++ packages/nextjs/components/Torrent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ProgressBar from './ProgressBar';
 import seedingSVG from '../static/seeding.svg';
 import downloadingSVG from '../static/downloading.svg';
 
@@ -82,6 +81,50 @@ const Info = (props) => {
           margin-bottom: -3px;
         }
         `}</style>
+    </div>
+  );
+};
+
+const ProgressBar = ({
+  className,
+  color,
+  progress,
+}) => {
+  const progressBarClass = className ? `progress-bar ${className}` : 'progress-bar';
+  return (
+    <div className="status rounded mt-1">
+      <div className={`${progressBarClass} rounded`}>
+        <span>
+          {progress.toFixed(2)}%
+        </span>
+      </div>
+      <style jsx>{`
+        .progress-bar {
+          width: ${progress}%;
+          background-color: ${color || 'var(--primary)'};
+          height: 20px;
+          background-image: linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent);
+          background-size: 1rem 1rem;
+          animation-name: progress-bar-stripes;
+          animation-duration: 3s;
+          animation-timing-function: linear;
+          animation-iteration-count: infinite;
+          text-align: center;
+          min-width: 50px;
+          display: flex;
+          flex-direction: row;
+        }
+        @keyframes progress-bar-stripes {
+          from { background-position: 115px 0; }
+          to { background-position: 0 0; }
+        }
+        .rounded {
+          border-radius: 15px;
+        }
+        .status{
+          border: 2px solid lightgray;
+        }
+     `}</style>
     </div>
   );
 };
