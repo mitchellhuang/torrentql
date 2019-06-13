c480155b361c433ab956dd487eee74f079306a89
Mitchell Huang <mitchell@mitchellhuang.net>
2019-06-13 18:29:52 -0400

    Move client-side logout to componentDidMount

--- packages/nextjs/pages/logout.js
+++ packages/nextjs/pages/logout.js
@@ -5,14 +5,18 @@ import Router from 'next/router';
 class Logout extends Component {
   static async getInitialProps({ res, apolloClient }) {
     if (!process.browser) {
+      await apolloClient.resetStore();
       res.clearCookie('token');
       res.redirect('/login');
-    } else {
-      await apolloClient.resetStore();
-      document.cookie = cookie.serialize('token', '', { expires: new Date(0) });
-      Router.push('/login');
     }
     return {};
+  }
+
+  async componentDidMount() {
+    const { apolloClient } = this.props;
+    await apolloClient.resetStore();
+    document.cookie = cookie.serialize('token', '', { expires: new Date(0) });
+    Router.push('/login');
   }
 
   render() {
