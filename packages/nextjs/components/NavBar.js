9345e1caff70068096df1f9dd508cab7300996df
Patrick Krisko <31548362+pkrisko@users.noreply.github.com>
2019-06-14 04:06:41 -0400

    NavBarBurger (#36)
    * Updated global font, added lil navbar burger for mobile devices
    
    * updating
    
    * fixing
    
    * Removed burger class, refactored navbar entirely
    
    * minor refactor
    
    * removed font
    
    * Revert Modal back to master
    
    * Disable eslint for Modal
    
    * Cleanup
    
    * Fix wrapper padding with new NavBar

--- packages/nextjs/components/NavBar.js
+++ packages/nextjs/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { withRouter } from 'next/router';
 import { useQuery } from 'react-apollo-hooks';
@@ -7,32 +7,30 @@ import { Logo } from './Logo';
 
 const NavBar = ({ router }) => {
   const { data } = useQuery(IS_LOGGED_IN_QUERY);
-  const items = [{
-    name: 'Pricing',
-    url: '/pricing',
-  }, {
-    name: 'Features',
-    url: '/features',
-  }, {
-    name: 'API',
-    url: '/api',
-  }];
-  if (data && data.isLoggedIn) {
-    items.push({
-      name: 'Dashboard →',
-      url: '/dashboard',
-    });
-  } else {
-    items.push({
-      name: 'Log in →',
-      url: '/login',
-    });
-  }
+  const [open, setOpen] = useState(false);
+  const toggle = () => {
+    setOpen(!open);
+  };
+  const items = [
+    { name: 'Pricing', url: '/pricing' },
+    { name: 'Features', url: '/features' },
+    { name: 'API', url: '/api' },
+  ];
+  const finalItem = (data && data.isLoggedIn)
+    ? { name: 'Dashboard →', url: '/dashboard' }
+    : { name: 'Log in →', url: '/login' };
+  items.push(finalItem);
   return (
     <div className="navbar">
       <div className="wrapper">
-        <Logo className="logo" />
-        <ul className="tabs">
+        <div className="logo-burger-wrapper">
+          <Logo />
+          <NavBarBurger
+            open={open}
+            onClick={() => toggle()}
+          />
+        </div>
+        <ul className={open ? 'tabs' : 'tabs hidden'}>
           { items.map(item => (
             <li key={item.url}>
               <Link href={item.url}>
@@ -46,53 +44,145 @@ const NavBar = ({ router }) => {
       </div>
       <style jsx>{`
         .navbar {
-          padding: 15px 0;
+          display: block;
+          background: white;
+          position: sticky;
+          top: 0;
+          margin: 0;
+          width: 100vw;
+          box-shadow: #fff 0 -15px, rgba(0,0,0,0.1) 0 0 15px;
         }
         .wrapper {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
+          padding: 10px 15px;
         }
-        :global(.logo) {
-          margin-bottom: 15px;
+        .logo-burger-wrapper {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-between;
         }
         .tabs {
           list-style-type: none;
-          padding: 0;
           margin: 0;
+          padding: 0;
+          margin-top: 10px;
+        }
+        .tabs li {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border: 2px solid var(--lightGray);
+          border-radius: 5px;
+          padding: 8px;
+        }
+        .tabs li:not(:last-child) {
+          margin-bottom: 8px;
+        }
+        .tabs li:last-child {
+          background-color: var(--primary);
+          border-color: var(--primary);
+        }
+        .tabs li:last-child a {
+          color: var(--white);
         }
         .tabs li a {
           display: inline-block;
+          color: var(--black);
           font-size: 18px;
           font-weight: 600;
           border-radius: 5px;
         }
-        .tabs li:not(:last-child) a {
-          margin-bottom: 5px;
+        .hidden {
+          display: none;
         }
         @media(min-width: 768px) {
           .wrapper {
             flex-direction: row;
             align-items: center;
           }
-          :global(.logo) {
-            margin-bottom: 0;
+          .logo-burger-wrapper :global(.burger) {
+            display: none;
+          }
+          .tabs {
+            display: flex;
+            align-items: center;
+            margin-top: 0;
           }
           .tabs li {
-            float: left;
+            display: inline-block;
+            border: none;
+            padding: 0;
           }
-          .tabs li a {
-            color: var(--black);
-            font-size: 16px;
-          }
-          .tabs li:not(:last-child) a {
+          .tabs li:not(:last-child) {
             margin-bottom: 0;
             margin-right: 15px;
+          }
+          .tabs li:last-child {
+            background-color: var(--white);
+          }
+          .tabs li:last-child a {
+            color: var(--black);
+          }
+          .tabs li a {
+            font-size: 18px;
+          }
+          .hidden {
+            display: block;
           }
         }
       `}</style>
     </div>
   );
 };
+
+const NavBarBurger = ({
+  open,
+  onClick,
+}) => (
+  <div
+    className="burger"
+    onClick={onClick}
+    onKeyPress={onClick}
+    role="button"
+    tabIndex="0"
+  >
+    <div className={open ? 'open bar' : 'bar'} />
+    <div className={open ? 'open bar' : 'bar'} />
+    <style jsx>{`
+      .burger {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        width: 40px;
+        height: 40px;
+        margin-right: -9px;
+        outline: 0;
+      }
+      .bar {
+        width: 22px;
+        height: 1px;
+        background: var(--black);
+        transition: transform 550ms ease;
+      }
+      .bar:first-child {
+        transform: translateY(-4px);
+      }
+      .bar:last-child {
+        transform: translateY(4px);
+      }
+      .open:first-child {
+        transform: translateY(1px) rotate(45deg);
+      }
+      .open:last-child {
+        transform: rotate(-45deg);
+      }
+    `}</style>
+  </div>
+);
 
 export default withRouter(NavBar);
