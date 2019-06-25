d833e23dc2365f482a2cbc074fd52a3848698189
pkrisko <patrick.krisko@gmail.com>
2019-06-24 21:41:49 -0400

    added tsconfig and tslint, renamed extensions from .js to .tsx and .ts

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,5 +1,6 @@
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
+const withTypeScript = require("@zeit/next-typescript");
 
-module.exports = withPlugins([withCSS, withImages]);
+module.exports = withPlugins([withCSS, withImages, withTypeScript]);
