6237608eb7f7a91d17e0b100a83db2484213dda8
jofsky <josh_kofsky@gmail.com>
2019-07-04 14:11:55 -0700

    adding started markdown

--- packages/nextjs/pages/dashboard/downloads.tsx
+++ packages/nextjs/pages/dashboard/downloads.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Dashboard from '../../layouts/Dashboard';
 import withAuth from '../../lib/withAuth';
+import Markdown from 'markdown-to-jsx';
+import React from 'react';
+import { render } from 'react-dom';
 
+
+
+/*
+    renders:
+
+    <h1>Hello world!</h1>
+ */
 const Downloads = () => (
   <Dashboard title="Downloads" noFooter>
     <div>
       Downloads.
     </div>
   </Dashboard>
+  render(<Markdown># Hello world!</Markdown>, document.body);
 );
 
 export default withAuth(Downloads);
