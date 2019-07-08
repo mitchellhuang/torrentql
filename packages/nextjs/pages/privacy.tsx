4d8fefe62a393486a2a3d6b774924075bd92d267
jofsky <josh_kofsky@gmail.com>
2019-07-07 21:27:47 -0700

    updating privacy policy need to review this more thoroughly

---
+++ packages/nextjs/pages/privacy.tsx
@@ -0,0 +1,149 @@
+import Markdown from 'markdown-to-jsx';
+import React from 'react';
+import { render } from 'react-dom';
+
+
+const privacyPolicy = () => (
+<>
+  <Markdown>
+	# Privacy Policy
+  </Markdown>
+  <hr/>
+  <Markdown>
+  ## What information do we collect?
+  </Markdown>
+  <Markdown>
+    We collect information from you when you register on our site, 
+    place an order, subscribe to our newsletter or fill out a form.  
+  </Markdown>
+  <Markdown>
+    When ordering or registering on our site, as appropriate, you 
+    may be asked to enter your: name, e-mail address, mailing address, 
+    phone number or credit card information. You may, however, visit 
+    our site anonymously.
+  </Markdown>
+  <br/>
+  <br/>
+  <Markdown>
+  ##  What do we use your information for?  
+  </Markdown>
+  <Markdown>
+  ####  Any of the information we collect from you may be used in 
+  one of the following ways:  
+  </Markdown>
+  <ul>
+    <li>
+      To personalize your experience (your information helps us to 
+      better respond to your individual needs).
+    </li>
+    <li>
+      To improve our website (we continually strive to improve our 
+      website offerings based on the information and feedback we receive 
+      from you).
+    </li>
+    <li>
+      To improve customer service (your information helps us to 
+      more effectively respond to your customer service requests 
+      and support needs).
+    </li>
+    <li>
+      To process transactions (We may share information with 
+      third parties for handling the processing of the payment).
+    </li>
+    <li>
+      To administer a contest, promotion, survey or other site feature.
+    </li>
+  </ul>
+  <Markdown>
+    To send periodic emails The email address you provide for 
+    order processing, may be used to send you information and 
+    updates pertaining to your order, in addition to receiving 
+    occasional company news, updates, related product or service 
+    information, etc. Note: If at any time you would like to unsubscribe 
+    from receiving future emails, we include detailed unsubscribe 
+    instructions at the bottom of each email.  
+  </Markdown>
+  <Markdown>
+   ## How do we protect your information?  
+  </Markdown>
+  <Markdown>
+    We implement a variety of security measures to maintain the 
+    safety of your personal information when you place an order or 
+    enter, submit, or access your personal information.  
+  </Markdown>
+  <br/>
+  <br/>
+  <Markdown>
+    We offer the use of a secure server. All supplied 
+    sensitive/credit information is transmitted via Secure Socket 
+    Layer (SSL) technology and then encrypted into our Database to 
+    be only accessed by those authorized with special access rights 
+    to our systems, and are required to keep the information 
+    confidential.  
+  </Markdown>
+  <Markdown>
+    After a transaction, your private information 
+    (credit cards, social security numbers, financials, etc.) 
+    will not be stored on our servers.
+  </Markdown>
+  <Markdown>
+  ##  Do we use cookies?  
+  </Markdown>
+  <Markdown>
+    Yes (Cookies are small files that a site or its service provider 
+    transfers to your computers hard drive through your Web browser 
+    (if you allow) that enables the sites or service providers systems 
+    to recognize your browser and capture and remember certain 
+    information.
+  </Markdown>
+  <br/>
+  <br/>
+  <Markdown>
+    We use cookies to help us remember and process the items in your shopping cart, understand and save your preferences for future visits, keep track of advertisements and compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future. We may contract with third-party service providers to assist us in better understanding our site visitors. These service providers are not permitted to use the information collected on our behalf except to help us conduct and improve our business.  
+  </Markdown>
+
+  <Markdown>
+    Do we disclose any information to outside parties?  
+  </Markdown>
+
+  <Markdown>
+    We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.
+  </Markdown>
+  <Markdown>
+  ## Online Privacy Policy Only  
+  </Markdown>
+
+  <Markdown>
+    This online privacy policy applies only to information collected through our website and not to information collected offline.  
+  </Markdown>
+  <Markdown>
+  ## Terms and Conditions  
+  </Markdown>
+  <Markdown>
+    Please also visit our Terms and Conditions section establishing the use, disclaimers, and limitations of liability governing the use of our website at terms of service  
+  </Markdown>
+
+  <Markdown>
+  ## Your Consent  
+  </Markdown>
+
+  <Markdown>
+    By using our site, you consent to our web site privacy policy.
+  </Markdown>
+
+  <Markdown>
+  ## Changes to our Privacy Policy  
+  </Markdown>
+
+  <Markdown>
+    If we decide to change our privacy policy, we will update the Privacy Policy modification date below.  
+  </Markdown>
+  <hr/>
+  <Markdown>
+  #### Last Reviewed: July 7, 2019
+  </Markdown>
+  <hr/>
+</>
+);
+
+export default privacyPolicy;
