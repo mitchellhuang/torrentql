0d9ea0ce801724c26b5795e8a894fa0080a4bcd1
jofsky <josh_kofsky@gmail.com>
2019-07-08 22:54:18 -0700

    fixing

--- packages/nextjs/pages/terms.tsx
+++ packages/nextjs/pages/terms.tsx
@@ -8,12 +8,7 @@ const Terms = () => (
   <div id="terms">
     <Markdown children={terms} />
     <style jsx>{`
-      #terms {
-        padding: 10px 30px;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
-          Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
-          "Segoe UI Emoji", "Segoe UI Symbol"
-      }
+      
     `}</style>
   </div>
 </Main>
