97f454d1d1355b97c4c012b9b2bff3684812ef9a
pkrisko <patrick.krisko@gmail.com>
2019-07-15 22:58:10 -0400

    Added initial file

---
+++ packages/nextjs/pages/features.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from '../layouts/Main';
+
+const Features = () => (
+  <Main title="Features">
+    <div>New </div>
+  </Main>
+);
+
+export default Features;
