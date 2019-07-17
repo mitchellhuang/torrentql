424ed310c88ccfc6b2c4d8f30e77e704d29ade4c
jofsky <josh_kofsky@gmail.com>
2019-07-16 21:07:31 -0700

    adding start to file viewer:

--- packages/nextjs/pages/torrents/[torrentId].tsx
+++ packages/nextjs/pages/torrents/[torrentId].tsx
@@ -6,6 +6,7 @@ import { GET_TORRENT_QUERY } from '../../apollo/queries';
 import { Unstyled } from '../torrents';
 import Main from '../../layouts/Main';
 import Link from 'next/link';
+import { Folder, File as FileIcon } from 'react-feather';
 
 const Torrent = () => {
   const router = useRouter();
@@ -30,12 +31,13 @@ const Torrent = () => {
           <a>Back to torrents</a>
         </Link>
         <h2 className="name">{torrent.name}</h2>
-        {Object.keys(torrent).map(key => (
-          <div key={key}>
-            <span className="title">{key}: </span>
-            <span>{JSON.stringify(torrent[key])}</span>
-          </div>
-        ))}
+        <h3>Contents:</h3>
+        <h5>{JSON.stringify(torrent.files.contents.size)}</h5>
+        <Directory name={torrent.name} />
+        <File name={torrent.name} />
+        <h5>{JSON.stringify(torrent.files.contents)}</h5>
+        <br/>
+        <h5>{JSON.stringify(torrent.files)}</h5>
         <style jsx>{`
         .name {
           margin-bottom: 15px;
@@ -49,4 +51,31 @@ const Torrent = () => {
   );
 };
 
+const Directory = ({ name }) => (
+  <div className="directory">
+    <Folder className="folder" color="blue" />
+    <div>{name}</div>
+    <style jsx>{`
+      .directory {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+    `}</style>
+  </div>
+);
+
+const File = ({ name }) => (
+  <div className="file">
+    <line></line><FileIcon color="blue"/>
+    <div> {name}</div>
+    <style jsx>{`
+      .file {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+    `}</style>
+  </div>
+)
 export default withAuth(Torrent);
