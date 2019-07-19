ba53ed65f782782888f2684a5f90272931265ab5
pkrisko <patrick.krisko@gmail.com>
2019-07-19 00:29:16 -0400

    We got a dirty file explorer boys

--- packages/nextjs/pages/torrents/[torrentId].tsx
+++ packages/nextjs/pages/torrents/[torrentId].tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import withAuth from '../../lib/withAuth';
 import { useQuery } from 'react-apollo-hooks';
 import { GET_TORRENT_QUERY } from '../../apollo/queries';
 import { Unstyled } from '../torrents';
 import Main from '../../layouts/Main';
-import Link from 'next/link';
 import { Folder, File as FileIcon } from 'react-feather';
+import Button from '../../components/Button';
 
 const Torrent = () => {
   const router = useRouter();
@@ -29,19 +29,43 @@ const Torrent = () => {
   return (
     <Main title={torrent.name}>
       <div className="wrapper">
-        <Link href="../torrents">
-          <a>Back to torrents</a>
-        </Link>
+        <Button href="/torrents" white className="back-button" animate>Back to torrents</Button>
         <h2 className="name">{torrent.name}</h2>
-        <h3>Contents:</h3>
-        {directoryDive(torrent.files.contents[initialDir], initialDir, 0)}
+        <h3 className="mb-2">Files</h3>
+        <div className="card">
+          {directoryDive(torrent.files.contents[initialDir], initialDir, 0)}
+        </div>
         <br/>
         <style jsx>{`
         .name {
           margin-bottom: 15px;
         }
+        :global(a.back-button) {
+          width: 200px;
+          margin-bottom: 10px;
+        }
         .title {
           font-weight: bold;
+        }
+        .card {
+          border: 2px solid var(--darkGray);
+          border-radius: 5px;
+          padding: 5px;
+          height: 200px;
+          max-width: 600px;
+          overflow: scroll;
+        }
+        ::-webkit-scrollbar {
+          width: 10px;
+        }
+        ::-webkit-scrollbar-track {
+          background: #f1f1f1;
+        }
+        ::-webkit-scrollbar-thumb {
+        background: #888;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+        background: #555;
         }
       `}</style>
       </div>
@@ -49,54 +73,66 @@ const Torrent = () => {
   );
 };
 
-   function directoryDive(dictionary,key, depth) {
-        if (dictionary.type === 'file') {
-          return (
-            <div key={key}>
-              <File name={key} depth={depth} />
-            </div>
-          )
-        } else if (dictionary.type === 'dir') {
-          const contents = dictionary.contents;
-          if (contents) {
-            return (
-              <div key={key}>
-                <Directory name={key} depth={depth} />
-                {Object.keys(contents).map(key => directoryDive(contents[key], key, depth + 1))}
-              </div>
-            )
-          }
-        } 
-      }
-
-const Directory = ({ name, depth }) => {
-  const offSet = depth > 0 ? (depth * 20) + 20 : 0;
+ function directoryDive(dictionary, key, depth) {
+  if (dictionary.type === 'file') {
+    return (
+    <div key={key}>
+      <File name={key} depth={depth} />
+    </div>
+    );
+  }
+  const contents = dictionary.contents;
   return (
-  <div className="directory">
-    <Folder className="folder" color="blue" />
-    <span className="name">{name}</span>
-    <style jsx>{`
+    <div key={key}>
+      <Directory name={key} depth={depth}>
+        {Object.keys(contents).map(key => directoryDive(contents[key], key, depth + 1))}
+      </Directory>
+    </div>
+  );
+}
+
+const Directory = ({ name, depth, children }) => {
+  const [expanded, toggle] = useState(false);
+  const offSet = depth > 0 ? (depth * 5) + 5 : 0;
+  return (
+    <div className="directory">
+      <div className="row" onClick={() => toggle(!expanded)} role="button" tabIndex={0}>
+        <Folder className="folder" color="#a7b0bd" />
+        <span className="name">{name}</span>
+      </div>
+      {expanded && children}
+      <style jsx>{`
       .directory {
+        display: flex;
+        flex-direction: column;
+        margin-left: ${offSet}px;
+      }
+      :global(svg.folder) {
+        fill: #a7b0bd;
+      }
+      .row {
         display: flex;
         flex-direction: row;
         align-items: center;
-        margin-left: ${offSet}px;
+      }
+      .row:focus {
+        outline:0;
       }
       .name {
         margin-left: 3px;
       }
-    `}</style>
-  </div>
-)
+      `}</style>
+    </div>
+  );
 };
 
 const File = ({ name, depth }) => (
   <div className="file">
-    <FileIcon color="blue"/>
+    <FileIcon color="#51A4FB"/>
     <span className="name">{name}</span>
     <style jsx>{`
       .file {
-        margin-left: ${20 * (depth + 1)}px;
+        margin-left: ${5 * (depth + 1)}px;
         display: flex;
         flex-direction: row;
         align-items: center;
