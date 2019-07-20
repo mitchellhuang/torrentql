2ab2d846d9f4073749a19355856651db2a2bd851
Patrick Krisko <31548362+pkrisko@users.noreply.github.com>
2019-07-20 19:07:50 -0400

    Made tabs look different (#81)
    * amend and rebase
    
    * whoops

--- packages/nextjs/layouts/Dashboard.tsx
+++ packages/nextjs/layouts/Dashboard.tsx
@@ -20,23 +20,27 @@ const tabs = [{
 }];
 
 const Tabs = ({ router }) => (
-  <ul>
-    { tabs.map(item => (
-      <li key={item.url}>
-        <Link href={item.url}>
-          <a className={router.pathname === item.url ? 'active' : undefined}>
-            {item.name}
-          </a>
-        </Link>
-      </li>
-    )) }
+  <div className="tabs wrapper">
+    <ul>
+      {tabs.map(item => (
+        <li key={item.url}>
+          <Link href={item.url}>
+            <a className={router.pathname === item.url ? 'active' : undefined}>
+              {item.name}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
     <style jsx>{`
+      .tabs {
+        padding: 5px 5px 0 5px;
+      }
       ul {
         display: block;
         list-style-type: none;
         padding: 0;
         margin: 0;
-        border-bottom: 1px solid var(--gray);
         overflow: auto;
         white-space: nowrap;
       }
@@ -54,10 +58,7 @@ const Tabs = ({ router }) => (
       }
       li > .active {
         color: var(--black);
-        background: var(--dashboardBg);
-        border-color: var(--gray);
-        border-radius: 5px 5px 0 0;
-        border-bottom-color: var(--dashboardBg);
+        border-bottom: 2px solid var(--gray);
       }
       @media(min-width: 768px) {
         ul {
@@ -65,7 +66,7 @@ const Tabs = ({ router }) => (
         }
       }
     `}</style>
-  </ul>
+  </div>
 );
 
 const Dashboard = ({
@@ -77,9 +78,11 @@ const Dashboard = ({
   <Global backgroundColor="var(--dashboardBg)" {...props}>
     <Head title={title} />
     <NavBar />
+    <div className="tabs">
+      <Tabs router={router} />
+    </div>
     <div className="wrapper">
       <div className="main">
-        <Tabs router={router} />
         <div className="content">
           {children}
         </div>
@@ -90,9 +93,13 @@ const Dashboard = ({
         display: flex;
         flex-direction: column;
       }
+      .tabs {
+        border-top: 1px solid var(--gray);
+        background-color: white;
+        box-shadow: 0 5px 10px rgba(0,0,0,0.12);
+      }
       .content {
         border-radius: 5px;
-        padding-top: 15px;
         overflow: auto;
       }
     `}</style>
