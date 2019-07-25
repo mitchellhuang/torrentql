0f80c0a21960a2bfc4023567d3724c7f626396c2
jofsky <josh_kofsky@gmail.com>
2019-07-24 21:38:37 -0700

    adding for patty

--- packages/nextjs/pages/torrents/[torrentId].tsx
+++ packages/nextjs/pages/torrents/[torrentId].tsx
@@ -10,6 +10,88 @@ import MediaPlayer from '../../components/MediaPlayer';
 
 const PrettyPrintJson = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
 
+const TorrentInfo = ({ torrent }) =>  (
+  <div>
+    <div><span className="label">ID: </span>
+      {torrent.id}
+    </div>
+    <div><span className="label">Hash: </span>
+      {torrent.hash}
+    </div>
+    <div><span className="label">Name: </span>
+      {torrent.name}
+    </div>
+    <div><span className="label">State: </span>
+      {torrent.state}
+    </div>
+    <div><span className="label">Progress: </span>
+      {torrent.progress}
+    </div>
+
+
+    <div><span className="label">Ratio: </span>
+      {torrent.ratio}
+    </div>
+    <div><span className="label">Upload Speed: </span>
+      {torrent.uploadSpeed}
+    </div>
+    <div><span className="label">Download Speed: </span>
+      {torrent.downloadSpeed}
+    </div>
+    <div><span className="label">Eta: </span>
+      {torrent.eta}
+    </div>
+    <div><span className="label">Number of Peers: </span>
+      {torrent.numPeers}
+    </div>
+
+
+
+
+    <div><span className="label">Number of Seeds: </span>
+      {torrent.numSeeds}
+    </div>
+    <div><span className="label">Download Speed: </span>
+      {torrent.downloadSpeed}
+    </div>
+    <div><span className="label">Eta: </span>
+      {torrent.eta}
+    </div>
+    <div><span className="label">Number of Peers: </span>
+      {torrent.numPeers}
+    </div>
+    <div><span className="label">Number of Seeds: </span>
+      {torrent.numSeeds}
+    </div>
+
+
+    <div><span className="label">Total Peers: </span>
+      {torrent.numSeeds}
+    </div>
+    <div><span className="label">Download Speed: </span>
+      {torrent.downloadSpeed}
+    </div>
+    <div><span className="label">Eta: </span>
+      {torrent.eta}
+    </div>
+    <div><span className="label">Number of Peers: </span>
+      {torrent.numPeers}
+    </div>
+    <div><span className="label">Number of Seeds: </span>
+      {torrent.numSeeds}
+    </div>
+
+
+    <style jsx>{`
+       .label {
+         background-color: white;
+         font-weight: bold;
+       }
+      `}</style>
+  </div>
+
+  );
+
 const TorrentWithData = ({ id }) => {
   const { loading, data, error } = useQuery(GET_TORRENT_QUERY, {
     ssr: false,
@@ -28,6 +110,7 @@ const TorrentWithData = ({ id }) => {
       <h2 className="mb-2">{torrent.name}</h2>
       <h3 className="mb-2">Info</h3>
       <div className="info">
+        <TorrentInfo torrent={torrent}/>
         <PrettyPrintJson data={torrent} />
       </div>
       <h3 className="mb-2">Files</h3>
