c167b7d6579e6fdd2f390729de038708dd34430e
Mitchell Huang <mitchell@mitchellhuang.net>
2019-07-28 02:10:47 -0700

    Add big sql query to writeBillingHistory

--- packages/billing/src/server.ts
+++ packages/billing/src/server.ts
@@ -1,14 +1,17 @@
 import 'dotenv/config';
 import { createConnectionFromEnv } from '@torrentql/common/dist/lib/db';
 import { Torrent } from '@torrentql/common/dist/entities/Torrent';
+import { User } from '@torrentql/common/dist/entities/User';
 import { BillingActivity } from '@torrentql/common/dist/entities/BillingActivity';
 import { BillingHistory } from '@torrentql/common/dist/entities/BillingHistory';
 import { mapDelugeToTorrent } from '@torrentql/common/dist/lib/deluge';
 
 const DISK_USAGE_GB_MONTH_COST = 0.01;
 const DISK_USAGE_GB_SECOND_COST = DISK_USAGE_GB_MONTH_COST / 30 / 86400;
-// const DATA_TRANSFER_IN_GB_COST = 0.00;
-// const DATA_TRANSFER_OUT_GB_COST = 0.01;
+const DATA_TRANSFER_IN_GB_COST = 0.01;
+const DATA_TRANSFER_OUT_GB_COST = 0.01;
+const DATA_TRANSFER_IN_BYTES_COST = DATA_TRANSFER_IN_GB_COST / 1e9;
+const DATA_TRANSFER_OUT_BYTES_COST = DATA_TRANSFER_OUT_GB_COST / 1e9;
 
 interface TorrentUsage {
   diskUsage: number;
@@ -80,25 +83,80 @@ const run = async () => {
     const beginAt = new Date();
     beginAt.setHours(endAt.getHours() - 1);
 
-    const billingActivities = await connection
-      .getRepository(BillingActivity)
-      .createQueryBuilder('billing_activity')
-      .select('torrent_id, SUM(data_transfer_out) as data_transfer_out')
-      .where('created_at >= :beginAt', { beginAt })
-      .andWhere('created_at < :endAt', { endAt })
-      .groupBy('torrent_id')
-      .getSql();
+    interface UsageByUser {
+      user_id: string;
+      begin_at: Date;
+      end_at: Date;
+      disk_usage: number;
+      data_transfer_in: number;
+      data_transfer_out: number;
+    }
 
-    console.log(billingActivities);
+    const usageByUser: UsageByUser[] = await connection
+      .createEntityManager()
+      .query(`
+        WITH per_torrent AS (
+          SELECT DISTINCT ON (t1.torrent_id)
+              t1.torrent_id,
+              t1.user_id,
+              t1.disk_usage,
+              CURRENT_TIMESTAMP as begin_at,
+              CURRENT_TIMESTAMP - interval '1 hour' as end_at,
+              MAX(t2.data_transfer_in) - MIN(t2.data_transfer_in) AS data_transfer_in,
+              MAX(t2.data_transfer_out) - MIN(t2.data_transfer_out) AS data_transfer_out
+          FROM
+              billing_activity AS t1,
+              billing_activity AS t2
+          WHERE
+              t1.torrent_id = t2.torrent_id
+              AND t1.created_at <= CURRENT_TIMESTAMP
+              AND t1.created_at >= CURRENT_TIMESTAMP - interval '1 hour'
+          GROUP BY
+              t1.disk_usage,
+              t1.torrent_id,
+              t1.user_id
+        ) SELECT
+            user_id,
+            begin_at,
+            end_at,
+            SUM(disk_usage) disk_usage,
+            SUM(data_transfer_in) data_transfer_in,
+            SUM(data_transfer_out) data_transfer_out
+            FROM per_torrent
+          GROUP BY
+            user_id,
+            begin_at,
+            end_at
+      `);
+
+    await connection.transaction(async (transaction) => {
+      await Promise.all(
+        usageByUser.map((user) => {
+          const totalCost = user.data_transfer_in * DATA_TRANSFER_IN_BYTES_COST +
+                            user.data_transfer_out * DATA_TRANSFER_OUT_BYTES_COST;
+          if (totalCost > 0) {
+            const tmpUser = new User();
+            tmpUser.id = user.user_id;
+            return transaction.createQueryBuilder().insert().into(BillingHistory).values({
+              totalCost,
+              beginAt: user.begin_at,
+              endAt: user.end_at,
+              user: Promise.resolve(tmpUser),
+            }).execute();
+          }
+          return Promise.resolve(undefined);
+        }),
+      );
+    });
   };
-
-  // writeBillingHistory();
 
   // writeBillingActivity();
 
+  // writeBillingHistory();
+
   setInterval(writeBillingActivity, 1 * 1000);
 
-  // setInterval(writeBillingHistory, 5 * 1000);
+  setInterval(writeBillingHistory, 60 * 1000);
 };
 
 run();
