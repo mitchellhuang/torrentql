a554d9bda33309a598b0f44a3c9630fe65074da2
pkrisko <patrick.krisko@gmail.com>
2019-08-02 17:11:11 -0400

    use ternary

--- packages/nextjs/layouts/Account.tsx
+++ packages/nextjs/layouts/Account.tsx
@@ -6,113 +6,108 @@ import Card from '../components/Card';
 
 const tabLinks = [
   {
-    path: 'usage',
+    url: '/account/usage',
     name: 'Usage',
     icon: <Database />,
   },
   {
-    path: 'update-email',
+    url: '/account/update-email',
     name: 'Update email',
     icon: <Mail />,
   },
   {
-    path: 'update-password',
+    url: '/account/update-password',
     name: 'Update password',
     icon: <Lock />,
   },
   {
-    path: 'logout',
+    url: '/account/logout',
     name: 'Logout',
     icon: <LogOut />,
   },
 ];
-const Account = ({ children, router }) => {
-  const selectedPath = router.pathname.split('/')[2];
-  return (
-    <Dashboard title="Account">
-      <div className="container">
-        <ul className="sidebar">
-          {tabLinks.map((tabLink) => {
-            const url = `/account/${tabLink.path}`;
-            return (
-              <li key={url} className={tabLink.path}>
-                <a href={url}>
-                  {tabLink.icon}
-                  <span className="tab-text ml-2">
-                  {tabLink.name}
-                </span>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="actions">
-          <Card className="selected-tab">
-            {children}
-          </Card>
-        </div>
+const Account = ({ children, router }) => (
+  <Dashboard title="Account">
+    <div className="container">
+      <ul className="sidebar">
+        {tabLinks.map(tabLink => (
+          <li key={tabLink.url} className={router.pathname.includes(tabLink.url) ? 'selected' : null}>
+            <a href={tabLink.url}>
+              {tabLink.icon}
+              <span className="tab-text ml-2">
+              {tabLink.name}
+            </span>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="actions">
+        <Card className="selected-tab">
+          {children}
+        </Card>
       </div>
-      <style jsx>{`
+    </div>
+    <style jsx>{`
+      .container {
+        display: flex;
+        flex-direction: column;
+      }
+      span.bar {
+        height: 22px;
+        width: 3px;
+      }
+      .selected .bar {
+        background-color: var(--black);
+      }
+      .selected a {
+        color: var(--black);
+      }
+      li, .selected-tab {
+        display: flex;
+        flex-direction: row;
+        color: black;
+        list-style-type: none;
+      }
+      a {
+        text-transform: capitalize;
+        font-weight: 600;
+        display: flex;
+        align-items: center;
+        color: var(--primary);
+        font-size: 12pt;
+      }
+      li:not(:last-child) {
+        margin-bottom: 15px;
+      }
+      .container :global(.selected-tab) {
+        flex: 1;
+      }
+      .sidebar {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+      }
+      .actions {
+        display: flex;
+        justify-content: center;
+        flex: 5;
+        padding: 0;
+        margin-top: 15px;
+      }
+      .help {
+        padding: 0 15px;
+      }
+      @media(min-width: 768px) {
         .container {
-          display: flex;
-          flex-direction: column;
-        }
-        span.bar {
-          height: 22px;
-          width: 3px;
-        }
-        .${selectedPath} .bar {
-          background-color: var(--black);
-        }
-        .${selectedPath} a {
-          color: var(--black);
-        }
-        li, .selected-tab {
-          display: flex;
           flex-direction: row;
-          color: black;
-          list-style-type: none;
-        }
-        a {
-          text-transform: capitalize;
-          font-weight: 600;
-          display: flex;
-          align-items: center;
-          color: var(--primary);
-          font-size: 12pt;
-        }
-        li:not(:last-child) {
-          margin-bottom: 15px;
-        }
-        .container :global(.selected-tab) {
-          flex: 1;
-        }
-        .sidebar {
-          padding: 0;
-          margin: 0;
-          height: 100%;
         }
         .actions {
-          display: flex;
-          justify-content: center;
-          flex: 5;
-          padding: 0;
-          margin-top: 15px;
-        }
-        .help {
+          margin-top: 0;
           padding: 0 15px;
         }
-        @media(min-width: 768px) {
-          .container {
-            flex-direction: row;
-          }
-          .actions {
-            padding: 0 15px;
-          }
-        }
-      `}</style>
-    </Dashboard>
-  );
-};
+      }
+    `}</style>
+  </Dashboard>
+);
 
 export default withRouter(Account);
