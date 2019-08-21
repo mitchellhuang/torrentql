b513174e3a1b66b2a800d1644858e996a0f7802b
Mitchell Huang <mitchell@mitchellhuang.net>
2019-08-20 18:32:02 -0700

    Move ApiKey mutations into ApiKeyResolver.ts

---
+++ packages/graphql/src/resolvers/ApiKeyResolver.ts
@@ -0,0 +1,60 @@
+import { Repository } from 'typeorm';
+import {
+  Args,
+  ArgsType,
+  Field,
+  Mutation,
+  Ctx,
+  Authorized,
+} from 'type-graphql';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+import { IsUUID } from 'class-validator';
+import nanoid from 'nanoid';
+import { ApiKey } from '@torrentql/common/dist/entities/ApiKey';
+import { Context } from '../lib/context';
+
+@ArgsType()
+class CreateApiKeyInput {
+  @Field()
+  name: string;
+}
+
+@ArgsType()
+class DeleteApiKeyInput {
+  @Field()
+  @IsUUID()
+  id: string;
+}
+
+export class ApiKeyResolver {
+  @InjectRepository(ApiKey)
+  private apiKeyRepository: Repository<ApiKey>;
+
+  @Authorized()
+  @Mutation(returns => ApiKey)
+  async createApiKey(
+    @Args() { name }: CreateApiKeyInput,
+    @Ctx() ctx: Context,
+  ) {
+    const apiKey = new ApiKey();
+    apiKey.key = nanoid();
+    apiKey.name = name;
+    apiKey.user = Promise.resolve(ctx.user);
+    await this.apiKeyRepository.save(apiKey);
+    return apiKey;
+  }
+
+  @Authorized()
+  @Mutation(returns => Boolean)
+  async deleteApiKey(
+    @Args() { id }: DeleteApiKeyInput,
+    @Ctx() ctx: Context,
+  ) {
+    const apiKey = await this.apiKeyRepository.findOne(id);
+    if (apiKey) {
+      await this.apiKeyRepository.remove(apiKey);
+      return true;
+    }
+    return false;
+  }
+}
