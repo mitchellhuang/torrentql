873bfdc469914a38450f4dba492c26b434db93ff
Mitchell Huang <mitchell@mitchellhuang.net>
2019-08-21 11:04:38 -0700

    Add createBitcoinTransaction mutation to frontend

--- packages/nextjs/pages/account/billing.tsx
+++ packages/nextjs/pages/account/billing.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useQuery } from 'react-apollo-hooks';
+import { useQuery, useMutation } from 'react-apollo-hooks';
 import Account from '../../layouts/Account';
 import withAuth from '../../lib/withAuth';
 import Card from '../../components/Card';
 import Button from '../../components/Button';
 import { ME_QUERY } from '../../apollo/queries';
+import { CREATE_BITCOIN_TRANSACTION_MUTATION } from '../../apollo/mutations';
 import { LoadingState, EmptyState } from '../../components/State';
 
 const Balance = ({
@@ -13,26 +14,37 @@ const Balance = ({
 }) => (
   <Card title="Billing" {...props}>
     <h2 className="mb-2">${balance.toFixed(2)}</h2>
-    <p>Your current account balance.</p>
+    <p className="footnote">Your current account balance.</p>
   </Card>
 );
 
-const Recharge = props => (
-  <Card title="Recharge Account" {...props}>
-    <div>
-      <Button className="mr-2" outline>$20</Button>
-      <Button className="mr-2" outline>$40</Button>
-      <Button className="mr-2" outline>$60</Button>
-      <Button className="mr-2" outline>$80</Button>
-      <Button outline>$100</Button>
-    </div>
-    <p className="mt-2">Recharge your account balance with Bitcoin.</p>
-  </Card>
-);
+const Recharge = (props) => {
+  const [createBitcoinTransaction, { loading, data }] = useMutation(CREATE_BITCOIN_TRANSACTION_MUTATION);
+  const bitcoinTransaction = data && data.createBitcoinTransaction;
+  const handleClick = amount => createBitcoinTransaction({ variables: { amount } });
+  return (
+    <Card title="Recharge Account" {...props}>
+      <div>
+        <Button className="mr-2" onClick={() => handleClick(20)} outline>$20</Button>
+        <Button className="mr-2" onClick={() => handleClick(40)} outline>$40</Button>
+        <Button className="mr-2" onClick={() => handleClick(60)} outline>$60</Button>
+        <Button className="mr-2" onClick={() => handleClick(80)} outline>$80</Button>
+        <Button onClick={() => handleClick(100)} outline>$100</Button>
+      </div>
+      { loading ?
+          <p className="mt-2">Generating invoice...</p> :
+          bitcoinTransaction &&
+            <p className="mt-2">
+              <a href={bitcoinTransaction.invoiceUrl} target="_blank">Pay with Bitcoin</a>
+            </p> }
+      <p className="footnote mt-2">Recharge your account balance with Bitcoin.</p>
+    </Card>
+  );
+};
 
 const Automatic = props => (
   <Card title="Automatic Recharge (coming soon)" {...props}>
-    <p>
+    <p className="footnote">
       Automatically recharge your account balance when it falls below a certain amount.
     </p>
   </Card>
@@ -40,7 +52,7 @@ const Automatic = props => (
 
 const History = props => (
   <Card title="Transaction History" {...props}>
-    <p>
+    <p className="footnote">
       Your transaction history for the last 30 days.
     </p>
   </Card>
@@ -67,7 +79,7 @@ const Billing = () => {
         <History />
       </div>
       <style jsx>{`
-        div :global(p) {
+        div :global(.footnote) {
           color: var(--dark-gray);
         }
       `}</style>
