29a902ee4f6d21de0e2aaff68d6cd3fc9693773e
Mitchell Huang <mitchell@mitchellhuang.net>
2019-08-20 18:13:10 -0700

    Add basic billing page layout

--- packages/nextjs/pages/account/billing.tsx
+++ packages/nextjs/pages/account/billing.tsx
@@ -1,14 +1,78 @@
 import React from 'react';
+import { useQuery } from 'react-apollo-hooks';
 import Account from '../../layouts/Account';
 import withAuth from '../../lib/withAuth';
 import Card from '../../components/Card';
+import Button from '../../components/Button';
+import { ME_QUERY } from '../../apollo/queries';
+import { LoadingState, EmptyState } from '../../components/State';
 
-const Billing = () => (
-  <Account title="Billing">
-    <Card title="Billing">
-      Billing information will go here.
-    </Card>
-  </Account>
+const Balance = ({
+  balance,
+  ...props
+}) => (
+  <Card title="Billing" {...props}>
+    <h2 className="mb-2">${balance.toFixed(2)}</h2>
+    <p>Your current account balance.</p>
+  </Card>
 );
+
+const Recharge = props => (
+  <Card title="Recharge Account" {...props}>
+    <div>
+      <Button className="mr-2" outline>$20</Button>
+      <Button className="mr-2" outline>$40</Button>
+      <Button className="mr-2" outline>$60</Button>
+      <Button className="mr-2" outline>$80</Button>
+      <Button outline>$100</Button>
+    </div>
+    <p className="mt-2">Recharge your account balance with Bitcoin.</p>
+  </Card>
+);
+
+const Automatic = props => (
+  <Card title="Automatic Recharge (coming soon)" {...props}>
+    <p>
+      Automatically recharge your account balance when it falls below a certain amount.
+    </p>
+  </Card>
+);
+
+const History = props => (
+  <Card title="Transaction History" {...props}>
+    <p>
+      Your transaction history for the last 7 days.
+    </p>
+  </Card>
+);
+
+const Billing = () => {
+  const { loading, data } = useQuery(ME_QUERY, { ssr: false });
+  const me = data && data.me;
+  if (loading) {
+    return (
+      <Account title="Billing">
+        <Card title="Billing">
+          <LoadingState noPad />
+        </Card>
+      </Account>
+    );
+  }
+  return (
+    <Account title="Billing">
+      <div>
+        <Balance balance={me.balance} className="mb-3" />
+        <Recharge className="mb-3" />
+        <Automatic className="mb-3" />
+        <History />
+      </div>
+      <style jsx>{`
+        div :global(p) {
+          color: var(--dark-gray);
+        }
+      `}</style>
+    </Account>
+  );
+};
 
 export default withAuth(Billing);
