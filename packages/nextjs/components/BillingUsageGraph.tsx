8d34b1a25cf42f8ee0aee733924b9fa34dd39b17
pkrisko <patrick.krisko@gmail.com>
2019-08-28 23:50:26 -0400

    Moved query, added more graphs. Meh

---
+++ packages/nextjs/components/BillingUsageGraph.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from 'react-vis';
+
+const MINUTES_IN_HOUR = 60;
+const MILLISECONDS_IN_MINUTE = 60000;
+
+const generatePoints = (billingUsages, key) => {
+  const points = [];
+  const currTime = new Date().getTime();
+  for (let idx = 0; idx < MINUTES_IN_HOUR; idx += 1) {
+    const date1 = new Date(currTime - (MINUTES_IN_HOUR - idx + 1) * MILLISECONDS_IN_MINUTE);
+    const date2 = new Date(currTime - (MINUTES_IN_HOUR - idx) * MILLISECONDS_IN_MINUTE);
+    let sum = 0;
+    billingUsages.forEach((billingUsage) => {
+      const createdAt = new Date(billingUsage.createdAt);
+      if (createdAt > date1 && createdAt <= date2) {
+        sum += billingUsage[key] / 1000000;
+      }
+    });
+    points[idx] = { x: date1, y: sum };
+  }
+  return points;
+};
+
+const BillingUsageGraph = ({ billingUsages, graphType }) => {
+  return (
+    <XYPlot
+      width={300}
+      height={300}
+      xType={'time'}>
+      <HorizontalGridLines />
+      <LineSeries
+        data={generatePoints(billingUsages, graphType)}/>
+      <XAxis />
+      <YAxis />
+    </XYPlot>
+  );
+};
+
+export default BillingUsageGraph;
--- packages/nextjs/pages/account/usage.tsx
+++ packages/nextjs/pages/account/usage.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import Account from '../../layouts/Account';
 import withAuth from '../../lib/withAuth';
 import Card from '../../components/Card';
-import Graph from '../../components/Graph';
+import BillingUsageGraph from '../../components/BillingUsageGraph';
+import { GET_BILLING_USAGE_QUERY } from '../../apollo/queries';
 
-const Usage = () => (
-  <Account title="Usage">
-    <Card title="Usage">
-      <Graph />
-      Usage information will go here.
-    </Card>
-  </Account>
-);
+const Usage = () => {
+  const { data: { getBillingUsage } } = useQuery(GET_BILLING_USAGE_QUERY);
+  let content;
+  if (getBillingUsage) {
+    const keys = ['dataTransferOut', 'dataTransferIn', 'diskUsage'];
+    content = keys.map(key => <BillingUsageGraph billingUsages={getBillingUsage} graphType={key} key={key} />);
+  }
+  return (
+    <Account title="Usage">
+      <Card title="Usage">
+        {content}
+      </Card>
+    </Account>
+  );
+};
 
 export default withAuth(Usage);
