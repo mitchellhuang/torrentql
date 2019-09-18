b87d244966783f3457a15cfc3808b04e7025d9f5
jofsky <josh.kofsky@gmail.com>
2019-09-17 21:24:16 -0700

    pushing to fix up

--- packages/nextjs/components/GraphTabs.tsx
+++ packages/nextjs/components/GraphTabs.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const GraphTabs = () => {
   const links = ['data-transfer-in', 'data-transfer-out', 'data-storage'];
+  const [selected, setSelected] = useState();
   const content = links.map((key) => {
       const link = `/account/usage/${key}`;
+      console.log(selected);
+      // onClick={() => setSelected(key)}
+      const tab = `key ${key}`
       return (
         <>
-          <Link href={link}><div className="key">{key}</div></Link>
+          <Link href={link} >
+          <button className={tab} onClick={() => setSelected(key)}>
+            <div>{key}</div>
+          </button>
+          </Link>
           <style jsx>{`
+              .${key} {
+                display: flex;
+                flex: 1;
+                color: rgb(32, 126, 206);
+              }
               .key {
                 display: inline-flex;
                 padding: 10px;
