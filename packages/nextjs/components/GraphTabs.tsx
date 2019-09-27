df9085f5c45d6e63b7da6c57a5dff7838686b405
jofsky <josh.kofsky@gmail.com>
2019-09-27 12:55:56 -0700

    made tabs vertical for desktop view, stacked for mobile view. Adjusted max/min-width convention to follow min-width styling

--- packages/nextjs/components/GraphTabs.tsx
+++ packages/nextjs/components/GraphTabs.tsx
@@ -7,37 +7,35 @@ const GraphTabs = () => {
   const content = links.map((key) => {
       const link = `/account/usage/${key}`;
       console.log(selected);
-      // onClick={() => setSelected(key)}
-      const tab = `key ${key}`
+      const tab = `key ${key}`;
       return (
         <>
           <Link href={link} >
-          <button className={tab} onClick={() => setSelected(key)}>
+          <div className={tab} onClick={() => setSelected(key)}>
             <div>{key}</div>
-          </button>
+          </div>
           </Link>
           <style jsx>{`
-              .${key} {
-                display: flex;
-                flex: 1;
-                color: rgb(32, 126, 206);
-              }
-              .key {
-                display: inline-flex;
-                padding: 10px;
-                background-color: #F7F8FA;
-                color: rgb(32, 126, 206);
-
+                .key {
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  background-color: #F7F8FA;
+                  padding: 10px;
+                  color: rgb(32, 126, 206);
                 }
-                .key:hover {
-                  background-color: lightblue;
-                }
-                @media(max-width: 767px) {
-                  .key {
+                @media(min-width: 767px) {
+                  .${key} {
                     display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    padding: 10px;
+                    flex: 1;
+                    color: rgb(32, 126, 206);
+                  }
+                  .key {
+                    display: inline-flex;
+                    width: 30%;
+                  }
+                  .key:hover {
+                    background-color: lightblue;
                   }
                 }
           `}
